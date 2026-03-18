a89fbff97f7ad9d6ec66991f7a8c3d8ae5d7e560
Fabio Capela <capela625@gmail.com>
2026-03-18 16:31:02 +0000

    change date

--- constants.tsx
+++ constants.tsx
@@ -59,7 +59,7 @@ export const BLOG_POSTS: BlogPost[] = [
       </p>
     `,
     category: 'Mindset',
-    date: 'Mar 18, 2026',
+    date: 'Mar 17, 2026',
     author: 'Fabio',
     readTime: '6 min',
     imageUrl: 'https://images.unsplash.com/photo-1504194104404-433180773017?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
